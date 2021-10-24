806275126eed1f14259ced119954082d33dae809
jaka72 <jaka72@gmail.com>
2021-10-24 21:06:11 +0200

    removed folder bauer

--- example_bauer_mandelbrot/x_run_mandelbrot.sh
+++
@@ -1,7 +0,0 @@
-gcc -o mandelbrot -O4 x_mandelbrot_bauer.c 
-
-#./mandelbrot  <xmin>   <xmax>   <ymin>    <ymax>    <maxiter>  <xres>  <out.ppm>
-  ./mandelbrot 0.27085  0.27100  0.004640  0.004810  75         100    pic.ppm
-
-#  mimeopen -d pic.ppm
-display pic.ppm
