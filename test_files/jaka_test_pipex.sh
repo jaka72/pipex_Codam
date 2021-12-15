636280a363dde9a14bae5ffdeb45934fe23ca22e
Jaka Murovec banic <jmurovec@f0r2s5.codam.nl>
2021-12-15 13:19:41 +0100

    Changed the name of 'mycommand' file. Repairing the test file.

--- test_files/jaka_test_pipex.sh
+++ test_files/jaka_test_pipex.sh
@@ -2,47 +2,92 @@
 
 COL="3[4;34;37m"
 YEL="\033[1;33m"
-RES="\033[1;37m"
+RESET="\033[1;37m"
 LGREY="\033[0;37m"
 LCYAN="\033[1;36m"
+GRN="\033[38;2;57;181;74m"
+RED="\033[38;2;222;56;43m"
+RES="\033[0m"
 
 i=0
-echo -e "TEST ${i}:  infile cat 'wc -l' outfile\n  Shell:" > outfile
-cat infile | wc -l >> outfile
-echo "  Pipex:" >> outfile
-./pipex infile cat "wc -l" tempfile;   cat tempfile >> outfile
+echo -e "\nTEST ${i}:  infile cat 'wc -l' outfile"
+cat infile | wc -l > out_orig
+echo -e "   Expected exit code:" $?
+../pipex infile cat "wc -l" out_mine
+echo -e "   My exit code:      " $?
+if cmp --silent out_orig out_mine; then
+    printf "$GRN   OK, files are identical\n$RES"
+else
+    printf "$RED   Error, files are different\n$RES"
+fi
 
-i=$(( i+1 ))
-echo -e "\nTEST ${i}:  infile cat 'wc' outfile\n  Shell:" >> outfile
-cat infile | wc >> outfile
-echo "  Pipex:" >> outfile
-./pipex infile cat "wc" tempfile;   cat tempfile >> outfile
 
-i=$(( i+1 ))
-echo -e "\nTEST ${i}:  infile 'sleep 1' 'sleep 1' outfile\n  Shell:" >> outfile
-< infile sleep 0 | sleep 0 >> outfile
-echo "  Pipex:" >> outfile
-./pipex infile "sleep 0" "sleep 0" tempfile;   cat tempfile >> outfile
 
+# "cat -e" "grep gewoon"
 i=$(( i+1 ))
-echo -e "\nTEST ${i}:  infile cat 'grep six' outfile\n  Shell:" >> outfile
-< infile cat | grep 'six' >> outfile
-echo "  Pipex:" >> outfile
-./pipex infile cat 'grep six' tempfile;   cat tempfile >> outfile
+echo -e "\nTEST ${i}:  infile cat -e grep one outfile"
+< infile  cat -e |  grep one       > out_orig
+echo -e "   Expected exit code:" $?
+../pipex infile     "cat -e" "grep xone "    out_mine
+echo -e "   My exit code:      " $?
+if cmp --silent out_orig out_mine; then
+    printf "$GRN   OK, files are identical\n$RES"
+else
+    printf "$RED   Error, files are different\n$RES"
+fi
 
-# cat | grep -c hello
-i=$(( i+1 ))
-echo -e "\nTEST ${i}:  infile cat 'grep -c hello' outfile\n  Shell:" >> outfile
-< infile cat | grep -c 'hello' >> outfile
-echo "  Pipex:" >> outfile
-./pipex infile cat 'grep -c hello' tempfile;   cat tempfile >> outfile
 
-# head -c 12
-i=$(( i+1 ))
-echo -e "\nTEST ${i}:  infile 'head -c 12' cat outfile\n  Shell:" >> outfile
-< infile head -c 12 | cat  >> outfile
-echo "  Pipex:" >> outfile
-./pipex infile 'head -c 12' cat tempfile;   cat tempfile >> outfile
+
+
+# MUSTER MUSTER
+# i=$(( i+1 ))
+# echo -e "\nTEST ${i}:  infile       outfile"
+# cat infile            > out_orig
+# echo -e "   Expected exit code:" $?
+# ../pipex infile          out_mine
+# echo -e "   My exit code:      " $?
+# if cmp --silent out_orig out_mine; then
+#     printf "   OK, files are identical"
+# else
+#     printf "   Error, files are different "
+# fi
+
+
+
+
+
+
+# i=$(( i+1 ))
+# echo -e "\nTEST ${i}:  infile cat 'wc' outfile\n  Shell:" >> outfile
+# cat infile | wc >> outfile
+# echo "  Pipex:" >> outfile
+# ./pipex infile cat "wc" tempfile;   cat tempfile >> outfile
+
+# i=$(( i+1 ))
+# echo -e "\nTEST ${i}:  infile 'sleep 1' 'sleep 1' outfile\n  Shell:" >> outfile
+# < infile sleep 0 | sleep 0 >> outfile
+# echo "  Pipex:" >> outfile
+# ./pipex infile "sleep 0" "sleep 0" tempfile;   cat tempfile >> outfile
+
+# i=$(( i+1 ))
+# echo -e "\nTEST ${i}:  infile cat 'grep six' outfile\n  Shell:" >> outfile
+# < infile cat | grep 'six' >> outfile
+# echo "  Pipex:" >> outfile
+# ./pipex infile cat 'grep six' tempfile;   cat tempfile >> outfile
+
+# # cat | grep -c hello
+# i=$(( i+1 ))
+# echo -e "\nTEST ${i}:  infile cat 'grep -c hello' outfile\n  Shell:" >> outfile
+# < infile cat | grep -c 'hello' >> outfile
+# echo "  Pipex:" >> outfile
+# ./pipex infile cat 'grep -c hello' tempfile;   cat tempfile >> outfile
+
+# # head -c 12
+# i=$(( i+1 ))
+# echo -e "\nTEST ${i}:  infile 'head -c 12' cat outfile\n  Shell:" >> outfile
+# < infile head -c 12 | cat  >> outfile
+# echo "  Pipex:" >> outfile
+# ./pipex infile 'head -c 12' cat tempfile;   cat tempfile >> outfile
 
 
 # all lines with "hello" | print number, '-' , first item
